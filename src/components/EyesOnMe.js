c089fe499a5188cbd0f7b42496fa78497171aa80
vivianne <viviannjoroge91@gmail.com>
2023-01-13 13:17:40 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function focus() {
+        console.log("Good!");
+    }
+
+    function Blur() {
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <button onFocus={focus} onBlur={Blur}>
+            Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
